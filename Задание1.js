048ff937a66a56e04617ff2b0d85a7a3a7c0c25a
Ekaterina200316 <81128820+Ekaterina200316@users.noreply.github.com>
2021-03-25 14:02:37 +0700

    Add files via upload

---
+++ Задание1.js
@@ -0,0 +1,16 @@
+//BEGIN
+var a = 1;
+var b = 7;
+var sum = 0;
+
+while (a<b)
+{
+  a++;
+  
+  if(a<b) 
+    {
+      sum+=a;
+    }
+}    
+alert(sum);
+//END
---
+++ Задание2.js
@@ -0,0 +1,9 @@
+for(let i = 0; i < 10 / 2; ++i)
+{
+for(let j = 0; j < 20; ++j)
+{
+document.write("*");
+}
+document.write("<br>");
+
+}
